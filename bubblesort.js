1a6f608acce6b2db91329747e2c2d91db970cef7
Madeliene Owen <madeliene.owen@gmail.com>
2017-04-20 13:02:08 -0700

    Bare bones.

--- bubblesort.js
+++ bubblesort.js
@@ -10,10 +10,45 @@ function bubbleSort(array) {
           // if there have been no swaps, return sorted array
         // counter++;
   // return sorted array
+  if (array.length === 0) {
+    return [];
+  } else if (array.length === 1) { // ex: array: [6]
+    return array;
+  }
 
+  var sortedArray = [];
+  var sorted = false;
+  // array.forEach(function(element, index, array) {
+  //   if (element > array[index+1]) {
+
+  //   }
+  // });
+  // [4, 3, 2, 1]
+  // i: 0 -> 4,3 => 3,4 ==> [3, 4, 2, 1]
+  // i: 1 -> 4,2 => 2,4 ==> [3, 2, 4, 1]
+  // i: 2 -> 4,1 => 1,4 ==> [3, 2, 1, 4]
+  // i: 3 -> 
+
+  // [1,2,3,4]
+  while(!sorted) {
+    for(var i = 0; i < array.length; i++) { // [4, 3, 2, 1] length = 4
+      if (array[i] > array[i+1]) {
+        array = swap(array, i);
+        sorted = false;
+      } else {
+        sorted = true;
+        // if reached end of array AND...
+      }
+    }
+  }
+  console.log('~~~~~~~ ', array);
+  return array;
 }
 
-function swap(element1, element2) {
-
+function swap(array, index) { // [3,2,1]
+  var temp = array[index]; // 3
+  array[index] = array[index+1];
+  array[index+1] = temp;
+  return array;
   // helper function, spyOn to get # of bubbleSort swaps
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -2,20 +2,30 @@ describe('Bubble Sort', function(){
   //global variables
 
   beforeEach(function() {
-
+    
   });
 
-  it("is just a function, so it can contain any code", function() {
-    expect(foo).toEqual(1);
-  });
+  // it("is just a function, so it can contain any code", function() {
+  //   expect(foo).toEqual(1);
+  // });
 
-  it("can have more than one expectation", function() {
-    expect(foo).toEqual(1);
-    expect(true).toEqual(true);
-  });
+  // it("can have more than one expectation", function() {
+  //   expect(foo).toEqual(1);
+  //   expect(true).toEqual(true);
+  // });
 
   it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+    expect(bubbleSort([])).toEqual([]);
+  });
+
+  it('handles an array of 1 item', function(){
+    expect(bubbleSort([6])).toEqual([6]);
+  });
+
+  it('the size of the input array is the size of the output array', function(){
+    var input = [4, 3, 2, 1];
+    var output = bubbleSort(input);
+    expect(input.length).toEqual(output.length);
   });
 
   //expects sorted array
